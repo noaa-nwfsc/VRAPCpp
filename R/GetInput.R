a83daec427af27f91e28999e93f09f1c260748d9
Eli <eli.holmes@noaa.gov>
2018-01-18 22:37:43 -0800

    fix function names

--- R/GetInput.R
+++ R/GetInput.R
@@ -4,9 +4,10 @@
 #' @return Returns the list of all inputs
 GetInput = function(InFile){
   require(stringr)
-  postfix = str_split(InFile,"[.]")
+  postfix = str_split(InFile,"[.]")[[1]]
   postfix = postfix[length(postfix)]
   if(!(postfix %in% c("rav", "rap"))) stop("GetInput VRAP 2.0 requires either a .rav or .rap input file.")
-  if(postfix=="rav") inputs = ReadRav(InFile)
+  if(postfix=="rav") inputs = ReadRavFile(InFile)
+  if(postfix=="rap") inputs = ReadRapFile(InFile)
   inputs$InFile = InFile
 }
