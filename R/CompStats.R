29b1f4808e89e9a69e3f08c8251831709df2584e
Eli Holmes <eli.holmes@noaa.gov>
2018-01-19 17:37:04 -0800

    update

--- R/CompStats.R
+++ R/CompStats.R
@@ -1,120 +1,48 @@
 #' @title CompStats
-#' @description Returns the statistics (calculated values) needed to produce the summary output file
+#' @description Returns the statistics (calculated values) needed to produce the summary output files
 #' @details This function similar but not identical to the original VB function in VRAP
-#' @param BufNum Which simulation (TargetER or Pop) is this for.
 #' @param inputs Inputs from .rav file
-#' @param BufSRb Capacity.  Changes if StepFunc=Pop.  Otherwise stays the same.
-#' @param YearStats list of computed variables for each year: AEQMort, Escpmnt[Year,] = Escpmnt, TotAdultEscpmnt, TotAEQMort, TotEscpmnt,TempCohort.
-#' @param SummaryStats list with the summary statistics to be updated
-#' @return Updated SummaryStats list
-CompStatsEEH = function(BufNum, inputs, BufSRb, YearStats, SummaryStats){ 
-  #BufNum is BufMax, an integer
+#' @param SimResults A list from \link{RunSims2R} or \link{RunSims2C} with totEsc and totAEQMort for each ER/Pop value, each sim rep, and each year.
+#' @return SummaryStats and staticvars list
+CompStats = function(inputs, SimResults){ 
+
+  # Set up the SummaryStats
+  SummaryStats = list()
+  for(el in c("MinEscpmnt","MaxEscpmnt", "AvgEscpmnt"))
+    SummaryStats[[el]] = matrix(NA, inputs$BufMax, inputs$NYears)
+  for(el in c("MinBYrHR","MaxBYrHR","AvgBYrHR"))
+    SummaryStats[[el]] = matrix(NA, inputs$BufMax, inputs$NYears - (inputs$MaxAge - inputs$MinAge))
+  for(el in c("AvgAEQMort","AvgECrit","AvgCaHR", "BufAvgBYrHR", "PropExt", "PropRec", "BufAvgBYrHR", "AveRanFlow", "AveRanMarine"))
+    SummaryStats[[el]] = rep(NA, inputs$BufMax)
   
-  NYears = inputs$NYears
-  NRuns = inputs$NRuns
-  SRErrorA = inputs$SRErrorA
-  SRErrorA = inputs$SRErrorB
-  SRa = inputs$BSRa
-  if(inputs$StepFunc=="POP"){
-    SRb = BufSRb
-  }else{
-    SRB = inputs$BSRb
-  }
-  SRc = inputs$BSRc
-  SRd = inputs$BSRd
-  SurvScale = inputs$SurvScale
+  # Calculate the SummaryStats sensu VRAP 1.0
+  totEsc <- SimResults$totEsc 
+  totAEQmort <- SimResults$totAEQmort 
   
-  #In VB code Escapmnt (over ages) is redefined to be TotEscpmnt (sum over ages)
-  #kept terminology closer to what is being done
-  TotEscpmnt = YearStats$TotEscpmnt #a matrix of Escpmnt for each year
+  SummaryStats$AvgEscpmnt = apply(totEsc,c(1,3), mean)
+  SummaryStats$MinEscpmnt = apply(totEsc,c(1,3), min)
+  SummaryStats$MaxEscpmnt = apply(totEsc,c(1,3), max)
   
-  # COMPUTE STATISTICS
-  # Sum up over all reps and divide by reps
-  # COMPUTE AVERAGE MORTALITY OVER REPETITIONS
-  #divide by NRuns because we want the average over all runs
-  #Note in VB code, AEQMort is initially by age and then in SaveYearData, it is reassigned to be TotAEQMort.
-  #Here I keep AEQMort and TotAEQMort separate
-  #Thus the VB code refers to YearStats.AEQMort while VRAPR uses YearStats$TotAEQMort
-  SummaryStats$AvgAEQMort[BufNum] = SummaryStats$AvgAEQMort[BufNum]+mean(YearStats$TotAEQMort)/inputs$NRuns
+  SummaryStats$AvgAEQMort = apply(totAEQmort,c(1,3), mean)
   
-  # COMPUTE AVERAGE CALENDAR YEAR HARVEST RATE
-  #CalendarHR = YearStats$CalendarHR
-  #divide by NRuns because we want the average over all runs
-  SummaryStats$AvgCaHR[BufNum] = SummaryStats$AvgCaHR[BufNum] + mean(YearStats$CalendarHR)/inputs$NRuns
+  CalendarHR = totAEQmort/( totAEQmort + totEsc)
+  SummaryStats$AvgCaHR = apply(CalendarHR, 1, mean)
+
+  BelowECrit = totEsc < inputs$ECrit
+  SummaryStats$AvgECrit = apply(BelowECrit, 1, function(x){sum(x)/(inputs$NYears*inputs$NRuns)})
   
-  # COMPUTE PROPORTION OF TIMES ESCAPEMENT WAS LESS THAN LOWER LEVEL
-  # add 1/NRuns because we want the fraction out of all runs
-  SummaryStats$AvgECrit[BufNum] = SummaryStats$AvgECrit[BufNum] + sum(TotEscpmnt < inputs$ECrit)/(inputs$NYears*inputs$NRuns)
-  
-  # FIND MINIMUM, MAXIMUM, AND AVERAGE ESCAPEMENT        
-  SummaryStats$MinEscpmnt[BufNum,] = pmin(SummaryStats$MinEscpmnt[BufNum,],TotEscpmnt,na.rm=TRUE)
-  SummaryStats$MaxEscpmnt[BufNum,] = pmax(SummaryStats$MaxEscpmnt[BufNum,],TotEscpmnt,na.rm=TRUE)
-  #divide these by NRuns because we want the average over all runs
-  SummaryStats$AvgEscpmnt[BufNum, ] = SummaryStats$AvgEscpmnt[BufNum, ] + TotEscpmnt/inputs$NRuns
-  
-  
-  # COMPUTE PROPORTION OF TIMES ENDING ESCAPEMENT WENT BELOW QET (DL2) (depensation)
-  #uses last EndAv years
   assessmentYears = (inputs$NYears - (inputs$EndAv - 1)):inputs$NYears
-  # divide by NRuns at end to get frac of reps below
-  if(any(TotEscpmnt[assessmentYears] < (inputs$DL2 + 1))){
-    # add 1/NRuns because we want the fraction out of all runs
-    SummaryStats$PropExt[BufNum] = SummaryStats$PropExt[BufNum] + 1/inputs$NRuns
-  }
+  assesstotEsc = totEsc[,,assessmentYears]
+  tmp = apply( assesstotEsc,c(1,2), function(x){any(x<(inputs$DL2 + 1))} )
+  SummaryStats$PropExt = apply(tmp,1,sum)/inputs$NRuns
   
-  # COMPUTE PROPORTION OF TIMES ENDING ESCAPEMENT EXCEEDED UPPER LEVEL
-  EscpmntPositive = TotEscpmnt
+  EscpmntPositive = assesstotEsc
   EscpmntPositive[EscpmntPositive<1]=1
   #assessmentYears defined above; uses geometric mean over last EndAv years
   GeomeanEscpmnt = exp(mean(log(EscpmntPositive)[assessmentYears]))
-  if(GeomeanEscpmnt > inputs$ERecovery){
-    # add 1/NRuns because we want the fraction out of all runs
-    SummaryStats$PropRec[BufNum] = SummaryStats$PropRec[BufNum] + 1/inputs$NRuns
-  }
-  
-  # COMPUTE BROOD YEAR HARVEST RATES
-  BYrAEQMort = rep(0, inputs$NYears - (inputs$MaxAge-inputs$MinAge)) # ReDim BYrAEQMort(-1 To NYears% - 5)
-  BYrEscpmnt = rep(0, inputs$NYears - (inputs$MaxAge-inputs$MinAge)) #ReDim BYrEscpmnt(-1 To NYears% - 5)
-  # AGE 2
-  Age = 2
-  Years = 1:(inputs$NYears - (inputs$MaxAge-inputs$MinAge)) + (Age-inputs$MinAge)
-  Byrs = Years - Age
-  BYrAEQMort[Byrs+2] = BYrAEQMort[Byrs+2] + YearStats$AEQMort[Years,2]
-  BYrEscpmnt[Byrs+2] = BYrEscpmnt[Byrs+2] + YearStats$Escpmnt[Years,2]
-  
-  # AGE 3
-  Age= 3
-  Years = 1:(inputs$NYears - (inputs$MaxAge-inputs$MinAge)) + (Age-inputs$MinAge)
-  Byrs = Years - Age
-  BYrAEQMort[Byrs+2] = BYrAEQMort[Byrs+2] + YearStats$AEQMort[Years,3]
-  BYrEscpmnt[Byrs+2] = BYrEscpmnt[Byrs+2] + YearStats$Escpmnt[Years,3]
-  
-  # AGE 4
-  Age = 4
-  Years = 1:(inputs$NYears - (inputs$MaxAge-inputs$MinAge)) + (Age-inputs$MinAge)
-  Byrs = Years - Age
-  BYrAEQMort[Byrs+2] = BYrAEQMort[Byrs+2] + YearStats$AEQMort[Years,4]
-  BYrEscpmnt[Byrs+2] = BYrEscpmnt[Byrs+2] + YearStats$Escpmnt[Years,4]
-  
-  # AGE 5
-  Age = 5
-  Years = 1:(inputs$NYears - (inputs$MaxAge-inputs$MinAge)) + (Age-inputs$MinAge)
-  Byrs = Years - Age
-  BYrAEQMort[Byrs+2] = BYrAEQMort[Byrs+2] + YearStats$AEQMort[Years,5]
-  BYrEscpmnt[Byrs+2] = BYrEscpmnt[Byrs+2] + YearStats$Escpmnt[Years,5]
-  
-  TempBYrHR = BYrAEQMort / (BYrAEQMort + BYrEscpmnt)
-  TempBYrHR[BYrAEQMort < 1E-16]=0
-  #divide these by NRuns because we want the average over all runs
-  
-  SummaryStats$BufAvgBYrHR[BufNum] = SummaryStats$BufAvgBYrHR[BufNum] + mean(TempBYrHR)/inputs$NRuns
-  SummaryStats$AvgBYrHR[BufNum, ] = SummaryStats$AvgBYrHR[BufNum, ] + TempBYrHR/inputs$NRuns
-  #don't divide these by NRuns because Max/Min
-  SummaryStats$MinBYrHR[BufNum, ] = pmin(SummaryStats$MinBYrHR[BufNum, ], TempBYrHR, na.rm=TRUE)
-  SummaryStats$MaxBYrHR[BufNum, ] = pmax(SummaryStats$MaxBYrHR[BufNum, ], TempBYrHR, na.rm=TRUE)
-  
-  SummaryStats$AveRanMarine[BufNum] = SummaryStats$AveRanMarine[BufNum] + mean(YearStats$RanMarine)/inputs$NRuns
-  SummaryStats$AveRanFlow[BufNum] = SummaryStats$AveRanFlow[BufNum] + mean(YearStats$RanFlow)/inputs$NRuns
+  tmp = apply( assesstotEsc,c(1,2), function(x){ exp(mean(log(x))) } )
+  tmp = tmp > inputs$ERecovery
+  SummaryStats$PropRec = apply(tmp,1,sum)/inputs$NRuns
   
   return(SummaryStats)
 }
